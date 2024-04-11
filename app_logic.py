1c9a7e8fa4a26cfc375529499877404d81f36683
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-04-11 17:24:01 +0800

    Modify script to open new cmd window

--- app_logic.py
+++ app_logic.py
@@ -1,12 +1,11 @@
 # from MediaAutomationScripts import VerifyVideo
 
 def call_verifier(file_path):
-    print("HELLO")
-    print(file_path)
     import subprocess
     python_interpreter = 'python'
     script_path = './MediaAutomationScripts/VerifyVideo.py'
-    subprocess.run([python_interpreter, script_path, '-f', file_path])
+    creation_flag = subprocess.CREATE_NEW_CONSOLE
+    subprocess.run([python_interpreter, script_path, '-f', file_path], creationflags=creation_flag)
 
 def check_for_ffmpeg_binaries():
     pass
