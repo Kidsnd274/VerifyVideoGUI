347bfb0e32ddb5b066ba04805b7cccbb68120bce
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-04-14 22:17:25 +0800

    Redid UI with custom frames and reorganized program

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import customtkinter as ctk
 from ui.mainwindow import MainWindow
 from util.app_logic import call_verifier
 
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("VerifyVideoGUI")
+        self.geometry("600x250")
+        self.resizable(False, False)
+        MainWindow(self, call_verifier)
+        
 def main():
     ctk.set_appearance_mode("System") # Probably want to put this in settings next time
     ctk.set_default_color_theme("blue")
-    app = ctk.CTk()
+    app = App()
     MainWindow(app, call_verifier)
     app.mainloop()
 
--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -4,46 +4,86 @@ from CTkListbox import *
 from tkinter import filedialog
 from util.verify_options_object import VerifyOptionsObject
 
-class MainWindow:
-    def __init__(self, root, verifier_function):
-        self.root = root
-        self.root.title("VerifyVideoGUI")
-        self.root.geometry("600x400")
-        self.verifier_function = verifier_function
-
-        self.check_path = ctk.StringVar(value="")
-        self.path_entry = ctk.CTkEntry(root, textvariable=self.check_path)
-        # self.path_entry.place(relx=0.5, rely=0.2, anchor=ctk.CENTER)
-        self.path_entry.pack()
+class OptionsCheckboxFrame(ctk.CTkFrame):
+    def __init__(self, master, options_dict):
+        super().__init__(master)
         
-        self.browse_button = ctk.CTkButton(root, text="Browse", command=self.browseFile)
-        # self.browse_button.place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
-        self.browse_button.pack()
+        self.title = ctk.CTkLabel(self, text="Options", fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
         
         # Skip ffmpeg check checkbox
-        self.skip_ffmpeg_check_checkbox = ctk.CTkCheckBox(root, text="Skip ffmpeg Check", onvalue="on", offvalue="off")
-        self.skip_ffmpeg_check_checkbox.pack()
-        # self.skip_ffmpeg_check_checkbox.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        self.skip_ffmpeg_check_checkbox = ctk.CTkCheckBox(self, text="Skip ffmpeg Check", onvalue="on", 
+                                                          offvalue="off", variable=options_dict['skip_ffmpeg_variable'])
+        self.skip_ffmpeg_check_checkbox.grid(row=1, column=0, padx=10, pady=(10,0), sticky='w')
         
         # Filter files suffix
-        self.filter_checkbox_state = ctk.StringVar(value="off")
         self.suffix_var = ctk.StringVar(value="_new")
-        self.filter_suffix_checkbox = ctk.CTkCheckBox(root, text="Filter out files with suffix ", command=self.toggle_filter_checkbox, variable=self.filter_checkbox_state, onvalue="on", offvalue="off")
-        self.filter_suffix_entry = ctk.CTkEntry(root, textvariable=self.suffix_var, placeholder_text="_new", state="disabled")
-        self.filter_suffix_checkbox.pack()
-        # self.filter_suffix_checkbox.place(relx=0.4, rely=0.6, anchor=ctk.CENTER)
-        # self.filter_suffix_entry.place(relx=0.7, rely=0.6, anchor=ctk.CENTER)
-        
-        # Verify Button
-        self.verify_button = ctk.CTkButton(root, text="Verify Video", command=self.on_verify_button_clicked)
-        # self.verify_button.place(relx=0.5, rely=0.7, anchor=ctk.CENTER)
-        self.verify_button.pack()
+        self.filter_suffix_checkbox = ctk.CTkCheckBox(self, text="Filter out files with '_new' suffix ", onvalue="on", 
+                                                      offvalue="off", variable=options_dict['filter_suffix_variable'])
+        self.filter_suffix_checkbox.grid(row=2, column=0, padx=10, pady=(10,10), sticky='w')
+        # self.filter_suffix_entry = ctk.CTkEntry(self, textvariable=self.suffix_var, placeholder_text="_new", state="disabled")
 
+class BottomFrame(ctk.CTkFrame):
+    def __init__(self, master, options_dict, verify_button_event):
+        super().__init__(master)
+        
+        self.grid_columnconfigure(1, weight=1)
+        
+        options_frame = OptionsCheckboxFrame(self, options_dict)
+        options_frame.grid(row=0, column=0, sticky="nsew")
+        
+        verify_button = ctk.CTkButton(self, text="Verify Video", command=verify_button_event)
+        verify_button.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+        
+class MiddleFrame(ctk.CTkFrame):
+    def __init__(self, master, path_variable):
+        bg_color = master.cget('background')  # Set the same background as the original window
+        super().__init__(master, fg_color=bg_color)
+        
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_configure(padx=50, pady=0, sticky="ew")
+        
+        path_label = ctk.CTkLabel(self, text="Folder Path: ")
+        path_label.grid(row=0, column=0)
+        
+        self.path_variable = path_variable
+        self.path_entry = ctk.CTkEntry(self, textvariable=self.path_variable)
+        self.path_entry.grid(row=0, column=1, padx=10, sticky="nsew")
+                
+        browse_button = ctk.CTkButton(self, text="\U0001F4C1", width=40, height=30, command=self.browseFile)
+        browse_button.grid(row=0, column=2, sticky="nsew")
+        
     def browseFile(self):
         file_path = filedialog.askdirectory()
         if file_path:
-            # Update the entry with the selected file path
-            self.check_path.set(file_path)
+            self.path_variable.set(file_path)  # Update the entry with the selected file path
+
+class MainWindow():
+    def __init__(self, root, verifier_function):
+        self.root = root
+        self.verifier_function = verifier_function
+        
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_rowconfigure(2, weight=1)
+        
+        self.folder_path_variable = ctk.StringVar(value="")
+        self.skip_ffmpeg_variable = ctk.StringVar(value="off")
+        self.filter_suffix_variable = ctk.StringVar(value="off")
+        options_dict = {
+            'skip_ffmpeg_variable': self.skip_ffmpeg_variable,
+            'filter_suffix_variable': self.filter_suffix_variable           
+        }
+        
+        title_font = ctk.CTkFont(size=24, weight="bold")
+        title_label = ctk.CTkLabel(root, text="VerifyVideoGUI", justify="center", font=title_font)
+        title_label.grid(row=0, column=0, padx=20, pady=10, sticky="ew", columnspan=100)
+
+        middle_frame = MiddleFrame(root, self.folder_path_variable)
+        middle_frame.grid(row=1)
+        
+        bottom_frame = BottomFrame(root, options_dict, self.on_verify_button_clicked)
+        bottom_frame.grid(row=2, column=0, padx=50, pady=(0, 10), sticky="ew")
 
     def toggle_filter_checkbox(self):
         if self.filter_suffix_checkbox.get() == "on":
@@ -54,9 +94,9 @@ class MainWindow:
             print("checkbox disabled")
 
     def on_verify_button_clicked(self):
-        folder_path = self.check_path.get()
-        ignore_ffmpeg = self.skip_ffmpeg_check_checkbox.get() == "on"
-        filter_staxrip_suffix = self.filter_suffix_checkbox.get() == "on"
+        folder_path = self.folder_path_variable.get()
+        ignore_ffmpeg = self.skip_ffmpeg_variable.get() == "on"
+        filter_staxrip_suffix = self.filter_suffix_variable.get() == "on"
         
         options = VerifyOptionsObject(folder_path, ignore_ffmpeg, filter_staxrip_suffix)
         self.verifier_function(options)
