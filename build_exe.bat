64bad8b5d4cac093105b72bac4c543f5d23e17e1
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-04-16 15:58:31 +0800

    Added readme to the build

--- build_exe.bat
+++ build_exe.bat
@@ -1,2 +1,3 @@
 call .\venv\Scripts\activate.bat
-pyinstaller --noconfirm --onedir --windowed --add-data "./MediaAutomationScripts;MediaAutomationScripts/" --add-data "./venv/Lib/site-packages/customtkinter;customtkinter/"  main.py
+pyinstaller --noconfirm --onedir --windowed --add-data "./MediaAutomationScripts;MediaAutomationScripts/" --add-data "./venv/Lib/site-packages/customtkinter;customtkinter/" --name VerifyVideoGUI main.py
+copy .\etc\readme.txt .\dist\VerifyVideoGUI
