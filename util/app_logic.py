d62fc57b247160eb34ded996d2b8b3e21ca550ab
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-04-16 15:18:41 +0800

    Update program to use script even while using pyinstaller

--- util/app_logic.py
+++ util/app_logic.py
@@ -1,5 +1,8 @@
 from util.verify_options_object import VerifyOptionsObject
 import threading
+import os
+import sys
+import subprocess
 
 def call_verifier(optionsObject, callbackFunction):
     thread = threading.Thread(target=start_subprocess, args=(optionsObject, callbackFunction, ), daemon=True)
@@ -10,9 +13,21 @@ def start_subprocess(optionsObject, callbackFunction):
     skip_ffmpeg = optionsObject.skip_ffmpeg
     filter_suffix = optionsObject.filter_staxrip_suffix
     
-    import subprocess
+    # Check if we are running as a bundled application
+    if getattr(sys, 'frozen', False):
+        # If the application is run as a bundle, the pyInstaller bootloader
+        # extends the sys module by a flag frozen=True and sets the app 
+        # path into variable _MEIPASS'.
+        application_path = sys._MEIPASS
+        print(application_path)
+    else:
+        # Otherwise, we are running in a normal Python environment
+        application_path = '.'
+
+    # Adjust the script path according to the environment
+    script_path = os.path.join(application_path, 'MediaAutomationScripts', 'VerifyVideo.py')
+    
     python_interpreter = 'python'
-    script_path = './MediaAutomationScripts/VerifyVideo.py'
     creation_flag = subprocess.CREATE_NEW_CONSOLE
     args = [
         python_interpreter,
